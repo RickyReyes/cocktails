a2cf3af5616abe1f03d5dffe089bd7a7179c4e22
RickyReyes <77644172+RickyReyes@users.noreply.github.com>
2022-06-09 01:16:05 -0700

    add ranking next. fix vermouth text color.

--- src/App.js
+++ src/App.js
@@ -7,161 +7,201 @@ export default function App() {
 		{
 			name: 'Negroni',
 			ingredients: ['gin', 'sweet vermouth', 'Campari'],
-      photo: './cocktail-photos/negroni.jpg'
+      photoString: 'bg-negroni'
 		},
 		{
 			name: 'Boulevardier',
 			ingredients: ['whiskey', 'sweet vermouth', 'Campari'],
-			photo: './cocktail-photos/negroni.jpg'
+			photoString: 'bg-boulevardier'
 		},
 		{
 			name: 'Margarita',
 			ingredients: ['tequila', 'triple sec', 'lime juice'],
-			photo: './cocktail-photos/negroni.jpg'
+			photoString: 'bg-margarita'
 		},
 		{
 			name: 'Americano',
 			ingredients: ['sweet vermouth', 'Campari', 'soda'],
-			photo: './cocktail-photos/negroni.jpg'
+			photoString: 'bg-americano'
 		},
 		{
 			name: 'Old Fashioned',
 			ingredients: ['whiskey', 'sugar cube', 'Angostura bitters'],
-			photo: './cocktail-photos/negroni.jpg'
+			photoString: 'bg-old-fashioned'
 		},
 		{
 			name: 'Moscow Mule',
 			ingredients: ['vodka', 'ginger beer', 'lime juice'],
-			photo: './cocktail-photos/negroni.jpg'
+			photoString: 'bg-moscow-mule'
 		},
 		{
 			name: 'Manhattan',
 			ingredients: ['whiskey, sweet vermouth, Angostura bitters'],
-			photo: './cocktail-photos/negroni.jpg'
+			photoString: 'bg-manhattan'
 		},
 		{
 			name: 'Last Word',
 			ingredients: ['gin', 'green chartreuse', 'maraschino liqueur', 'lime juice'],
-			photo: './cocktail-photos/negroni.jpg'
+			photoString: 'bg-last-word'
 		},
 		{
 			name: 'Martini',
 			ingredients: ['gin', 'dry vermouth'],
-			photo: './cocktail-photos/negroni.jpg'
+			photoString: 'bg-martini'
 		},
 		{
 			name: 'Gin & Tonic',
 			ingredients: ['gin', 'tonic'],
-			photo: './cocktail-photos/negroni.jpg'
+			photoString: 'bg-gin-and-tonic'
 		},
 		{
 			name: 'Southside',
-			ingredients: ['gin', 'lemon juice', 'mint', 'cucumber', 'simple syrup'],
-			photo: './cocktail-photos/negroni.jpg'
+			ingredients: ['gin', 'lemon juice', 'mint', 'simple syrup'],
+			photoString: 'bg-southside'
 		}
 	]
+
+	console.log(cocktails);
+
 	const categories = [
 		{
 			name: 'spirits',
 			items: ['gin', 'tequila', 'vodka', 'whiskey'],
-			tailwindBG: 'bg-amber-800'
+			tailwindBG: 'bg-amber-800',
+			tailwindBorder: 'border-amber-800',
+			hoverBorder: 'hover:border-amber-800',
+			hoverTextColor: 'hover:text-amber-800',
+			tailwindTextColor: 'text-amber-800'
 		},
 		{
 			name: 'vermouth',
 			items: ['sweet vermouth', 'dry vermouth'],
-			tailwindBG: 'bg-yellow-600'
+			tailwindBG: 'bg-orange-600',
+			tailwindBorder: 'border-orange-600',
+			hoverBorder: 'hover:border-orange-600',
+			hoverTextColor: 'hover:text-orange-600',
+			tailwindTextColor: 'text-orange-600'
 		},
 		{
 			name: 'liqueurs',
 			items: ['triple sec', 'Campari', 'green chartreuse', 'maraschino liqueur'],
-			tailwindBG: 'bg-red-700'
-
+			tailwindBG: 'bg-red-700',
+			tailwindBorder: 'border-red-700',
+			hoverBorder: 'hover:border-red-700',
+			hoverTextColor: 'hover:text-red-700',
+			tailwindTextColor: 'text-red-700'
+			
 		},
 		{
 			name: 'juices',
 			items: ['lime juice', 'lemon juice', 'orange juice'],
-			tailwindBG: 'bg-lime-600'
+			tailwindBG: 'bg-lime-600',
+			tailwindBorder: 'border-lime-600',
+			hoverBorder: 'hover:border-lime-600',
+			hoverTextColor: 'hover:text-lime-600',
+			tailwindTextColor: 'text-lime-600'
 		},
 		{
 			name: 'bubbles',
 			items: ['soda', 'tonic', 'ginger beer', 'cola'],
-			tailwindBG: 'bg-blue-800'
+			tailwindBG: 'bg-blue-800',
+			tailwindBorder: 'border-blue-800',
+			hoverBorder: 'hover:border-blue-800',
+			hoverTextColor: 'hover:text-blue-800',
+			tailwindTextColor: 'text-blue-800'
 		},
 		{
 			name: 'bitters',
 			items: ['Angostura bitters', "Peychaud's"],
-			tailwindBG: 'bg-red-400'
+			tailwindBG: 'bg-red-400',
+			tailwindBorder: 'border-red-400',
+			hoverBorder: 'hover:border-red-400',
+			hoverTextColor: 'hover:text-red-400',
+			tailwindTextColor: 'text-red-400'
 		},
 		{
 			name: 'miscellaneous',
 			items: ['sugar cube'],
-			tailwindBG: 'bg-emerald-800'
+			tailwindBG: 'bg-emerald-800',
+			tailwindBorder: 'border-emerald-800',
+			hoverBorder: 'hover:border-emerald-800',
+			hoverTextColor: 'hover:text-emerald-800',
+			tailwindTextColor: 'text-emerald-800'
 		}
 	]
 
 	const [currentCategory, setCurrentCategory] = useState(categories[0])
   const [selectedItems, setSelectedItems] = useState([])
-  const [possibleCocktails, setPossibleCocktails] =useState([])
+  const [possibleCocktails, setPossibleCocktails] = useState([])
 
-	const categoryItems = categories.map((category, idx) => (
-		<li onClick={() => setCurrentCategory(category)}
-        key={idx}>
-      <button 
-        className={"text-white cursor-pointer rounded-full py-2 px-4 lg:py-3 lg:px-6 lg:text-2xl mb-1 " + category.tailwindBG + (category.name == currentCategory.name ? " border-2 border-black" : "")}
-      >
-        {category.name}
-      </button>
-    </li>
-	))
+	const categoryElements = categories.map((category, idx) => {
+		return <li onClick={() => setCurrentCategory(category)} key={idx}>
+							<button 
+								className={"cursor-pointer rounded-full py-2 px-4 lg:py-3 lg:px-6 lg:text-lg font-bold mb-1 hover:underline " + category.tailwindBG + (category.name == currentCategory.name ? " bg-white border-2 " + category.tailwindBorder + " " + category.tailwindTextColor + "" : " text-white")}>
+									{category.name}
+								</button>
+						</li>
+	})
 
-	const currentItems = categories
-			.filter(category => category.name === currentCategory.name)[0]
-			.items.map((item, idx) => 
-      <li className="h-min" key={idx}>
-        <button 
-          onClick={() => selectItem(item, currentCategory.tailwindBG)}
-          className={"cursor-pointer rounded-full py-2 px-4 text-white lg:py-3 lg:px-6 lg:text-2xl " + currentCategory.tailwindBG + (selectedItems.includes(item) ? " border-2 border-black" : "")}>
-          {item}
-        </button>
-      </li>)
+	const itemElements = categories
+		.filter(category => category.name === currentCategory.name)[0]
+		.items.map((item, idx) => {
+			return <li className="h-min" key={idx}>
+							<button 
+								onClick={(e) => handleSelectItem(item, currentCategory.tailwindBG)}
+								className={"cursor-pointer font-bold rounded-full py-2 px-4 text-white lg:py-2 lg:px-4 lg:text-xl hover:bg-white " + 
+								currentCategory.tailwindBG + " " + currentCategory.hoverTextColor + " border-white border-2 " + currentCategory.hoverBorder}>
+								{item}
+							</button>
+						</li>
+		})
 
-
-useEffect(() => {
-	setPossibleCocktails(cocktails.filter(cocktail => {
-		if (selectedItems.every(item => cocktail.ingredients.includes(item))) {
-			return cocktail
-		}
-	}))
-}, [selectedItems])
-
-function selectItem(itemName, bgColor) {
-	/* if item is already in list, deselect it */
-    if (selectedItems.includes([itemName, bgColor])) {
-      let withoutDeselected = selectedItems.filter(item => item[0] !== itemName)
-      setSelectedItems(withoutDeselected)
-    } else {
-			setSelectedItems(prevItems => [...prevItems, [itemName, bgColor]])
-    }
-  }
-	
 	const selectedElements = selectedItems.map((item, idx) => {
-		console.log(item[0])
 		return (
-			<li className={"cursor-pointer rounded-full py-2 px-4 text-white lg:py-3 lg:px-6 lg:text-2xl " + item[1]} key={idx}>
+			<li onClick={() => handleSelectItem(item[0], item[1])}
+			className={"cursor-pointer font-bold rounded-full py-2 px-4 text-white lg:py-3 lg:px-6 lg:text-2xl " + item[1]} key={idx}>
 				{item[0]}
 			</li>
 		)
 	})
 
+	function handleSelectItem(itemName, bgColor) {
+		for (let i = 0; i < selectedItems.length; i++) {
+			if (selectedItems[i][0] == itemName) {
+				let filteredItems = selectedItems.filter(item => item[0] !== itemName)
+				setSelectedItems(filteredItems)
+				return;
+			}
+		}
+		setSelectedItems(prevItems => [...prevItems, [itemName, bgColor]])
+	}
+
 	const cocktailElements = possibleCocktails.map((cocktail, idx) => {
+		let missingIngredients = cocktail.ingredients.map(ingredient => {
+			return selectedItems.some(item => item.includes(ingredient)) ? "" : ingredient
+		}).filter(item => !!item)
     return (
-			<li className="w-full flex flex-col items-start rounded-xl border-2" key={idx}>
-				<div style={{backgroundImage: "url('./cocktail-photos/negroni.jpg')"}} className="rounded-t-xl bg-cover bg-no-repeat bg-center h-60 w-full"></div>
-				<h3 className="px-2 text-2xl font-bold">{cocktail.name}</h3>
+			<li className={"w-full flex flex-col items-start rounded-2xl border-4 " + (missingIngredients.length == 0 ? "border-8 border-green-400" : "")}
+			key={idx}>
+				<div className={"rounded-t-lg bg-cover bg-no-repeat bg-center h-60 w-full " + cocktail.photoString}></div>
+				<h3 className="px-2 pt-1 text-2xl font-bold">{cocktail.name}</h3>
 				<p className="px-2 text-left">{cocktail.ingredients.join(", ")}</p>
+				{missingIngredients.length > 0 && <h4 className="px-2 text-left">
+					<span className="font-bold">you're missing:</span> {missingIngredients.join(", ")}
+				</h4>}
+				<p></p>
 			</li>
 		)
   })
+
+	useEffect(() => {
+		setPossibleCocktails(cocktails.filter(cocktail => {
+			if (selectedItems.some(item => cocktail.ingredients.includes(item[0]))) {
+				return cocktail
+			}
+		}))
+	}, [selectedItems])
+	
 
 	return (
 		<main className="App w-screen min-w-screen flex flex-col items-center">
@@ -174,16 +214,17 @@ function selectItem(itemName, bgColor) {
 				</h2>
 
 				{/* categories and items */}
-				<div className="flex justify-center min-w-5/6 border-4"> 
+				<div className="flex justify-left w-full"> 
 					<ul className="flex flex-col items-start">
-						{categoryItems}
+						{categoryElements}
 					</ul>
 					<ul className="flex flex-wrap items-start gap-2 justify-start items-start h-min">
-						{currentItems}
+						{itemElements}
 					</ul>
 				</div>
 							
-				{selectedItems.length > 0 && <div className="w-full"> 
+				{selectedItems.length > 0 &&
+					<div className="w-full"> 
 							<h2 className="font-cursive text-3xl pb-3">
 								2. you've selected...
 							</h2>
@@ -193,7 +234,7 @@ function selectItem(itemName, bgColor) {
 							<h2 className="font-cursive text-3xl pb-3">
 								3. you can make...
 							</h2>
-							<ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 content-center">
+							<ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4 gap-4 content-center">
 								{cocktailElements}
 							</ul>
 						</div>}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    // extend: {
+    extend: {
+      backgroundImage: {
+        'americano': "url('./cocktail-photos/americano.jpg')",
+        'boulevardier': "url('./cocktail-photos/boulevardier.jpg')",
+        'negroni': "url('./cocktail-photos/negroni.jpg')",
+        'moscow-mule': "url('./cocktail-photos/moscow-mule.jpg')",
+        'last-word': "url('./cocktail-photos/last-word.jpg')",
+        'margarita': "url('./cocktail-photos/margarita.jpg')",
+        'boulevardier': "url('./cocktail-photos/boulevardier.jpg')",
+        'martini': "url('./cocktail-photos/martini.jpg')",
+        'gin-and-tonic': "url('./cocktail-photos/gin-and-tonic.jpg')",
+        'old-fashioned': "url('./cocktail-photos/old-fashioned.jpg')",
+        'southside': "url('./cocktail-photos/southside.jpg')"
+      },
       fontFamily: {
         'cursive': ['"Pacifico"', 'cursive'],
       }
-    // },
+    },
   },
   plugins: [],
 }
