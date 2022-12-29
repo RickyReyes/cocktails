2cb8d14ef676ffd72a6e59baf2f55c906cfa6a12
RickyReyes <77644172+RickyReyes@users.noreply.github.com>
2022-12-29 15:12:27 -0800

    search bar added to All page

--- src/routes/All.jsx
+++ src/routes/All.jsx
@@ -5,6 +5,7 @@ import ViewPill from "../components/ViewPill";
 
 const All = ({ cocktails, selectedTags }) => {
   const [allView, setAllView] = useState("gallery");
+  const [search, setSearch] = useState("");
   const navigate = useNavigate();
 
   const list = (
@@ -12,41 +13,48 @@ const All = ({ cocktails, selectedTags }) => {
       className="flex flex-col items-center
 			w-full my-4 mx-auto text-left"
     >
-      {cocktails.map((cocktail) => {
-        return (
-          <li key={Math.random()} className="text-3xl underline cursor-pointer">
-            <Link
-              to={
-                "/" +
-                cocktail.name
-                  .replace("#", "")
-                  .toLowerCase()
-                  .split(" ")
-                  .join("-")
-              }
+      {cocktails
+        .filter((cocktail) => cocktail.name.toLowerCase().includes(search))
+        .map((cocktail) => {
+          return (
+            <li
+              key={Math.random()}
+              className="text-3xl underline cursor-pointer"
             >
-              {cocktail.name}
-            </Link>
-          </li>
-        );
-      })}
+              <Link
+                to={
+                  "/" +
+                  cocktail.name
+                    .replace("#", "")
+                    .toLowerCase()
+                    .split(" ")
+                    .join("-")
+                }
+              >
+                {cocktail.name}
+              </Link>
+            </li>
+          );
+        })}
     </ul>
   );
 
   const cards = (
     <ul className="sm:w-full p-4 w-screen grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 content-center">
-      {cocktails.map((cocktail) => (
-        <CocktailCard
-          key={cocktail.name}
-          tags={cocktail.tags ?? []}
-          name={cocktail.name}
-          missing={[]}
-          onAllPage={true}
-          selectedTags={selectedTags}
-          ingredients={cocktail.ingredients}
-          photoString={cocktail.photoString}
-        />
-      ))}
+      {cocktails
+        .filter((cocktail) => cocktail.name.toLowerCase().includes(search))
+        .map((cocktail) => (
+          <CocktailCard
+            key={cocktail.name}
+            tags={cocktail.tags ?? []}
+            name={cocktail.name}
+            missing={[]}
+            onAllPage={true}
+            selectedTags={selectedTags}
+            ingredients={cocktail.ingredients}
+            photoString={cocktail.photoString}
+          />
+        ))}
     </ul>
   );
 
@@ -62,6 +70,26 @@ const All = ({ cocktails, selectedTags }) => {
         </h1>
       </div>
       <ViewPill view={allView} setView={setAllView} />
+      <label className="block">
+        Search:
+        <input
+          className="border border-black rounded p-1 mt-2 ml-2 focus:outline-none"
+          type="text"
+          name="search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+        />
+      </label>
+      {cocktails.filter((cocktail) =>
+        cocktail.name.toLowerCase().includes(search)
+      ).length === 0 && (
+        <div className="rounded-lg shadow-lg p-4 mt-4 mx-auto max-w-xs">
+          <h2 className="text-xl">
+            Sorry, we couldn't find any matches for <b>{search}</b>...
+          </h2>
+          <p>Please try searching for another term</p>
+        </div>
+      )}
       {allView === "list" ? { ...list } : { ...cards }}
     </div>
   );
